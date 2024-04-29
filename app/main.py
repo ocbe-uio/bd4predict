62f4e23ef2ce55dc64c97e772c9df2c6977e1ff9
Maurício Moreira-Soares <phydev@users.noreply.github.com>
2024-04-29 11:17:18 +0200

    port 8001 -> 8000

--- app/main.py
+++ app/main.py
@@ -78,4 +78,4 @@ if __name__ == "__main__":
     # run rest api with uvicorn
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
